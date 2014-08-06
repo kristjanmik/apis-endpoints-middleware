ebba4d3088972763298a9941a78a6e246cf288e9
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2014-08-06 16:44:26 +0000

    Added more endpoints

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = function(app){
     var map = {
         //prefix: module-name
-        'flights': 'apis-flights' 
+        'flights': 'apis-flights',
+        'car': 'apis-car',
+        'firm': 'apis-firm' 
     }
 
     for(var i in map){
