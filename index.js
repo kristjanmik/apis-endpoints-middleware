9939f2322bb1b9995a513a99f2085553d45f65c5
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2014-08-08 18:43:35 +0000

    ´Added more endpoints

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 module.exports = function(app){
     var map = {
-        //prefix: module-name
         'flights': 'apis-flights',
         'car': 'apis-car',
         'firm': 'apis-firm',
         'price': 'apis-price', 
         'currency': 'apis-currency', 
         'names': 'apis-names-lookup' ,
-        'particulates': 'apis-particulates'
+        'particulates': 'apis-particulates',
+        'cyclecounter': 'apis-cyclecounter'
     }
 
     for(var i in map){
-        require('domain')
-            .create()
-            .on('error', function(error) {
-                console.error('Error loading up endpoint error:');
-                console.error(error.stack)
-            })
-            .run(function() {
-                require(map[i])(app, '/' + i);
-            });
+        try{
+            require(map[i])(app, '/' + i);
+        }catch(error){
+            console.error('Error loading up endpoint error');
+            console.error(error.stack)
+        }
     }
 }
